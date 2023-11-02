0dca6ad210ecc3935b0fb578fe8c33aa0330e412
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-02 16:27:53 -0300

    feat: interface index

---
+++ src/interfaces/index.ts
@@ -0,0 +1,17 @@
+import {
+  TUserCreate,
+  TUserAdminOmit,
+  TUserUpdate,
+  TUserReturn,
+  TUserArrayReturn,
+  TUserRepo,
+} from "./users.interface";
+
+export {
+  TUserCreate,
+  TUserAdminOmit,
+  TUserUpdate,
+  TUserReturn,
+  TUserArrayReturn,
+  TUserRepo,
+};
