0d27a6145faf1c2610e20cb156354ad7d7d607e6
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-02 16:32:05 -0300

    feat: sechema index

---
+++ src/schemas/index.ts
@@ -0,0 +1,15 @@
+import {
+  userAdminOmitSchema,
+  userArrayReturnSchema,
+  userCreateSchema,
+  userReturnSchema,
+  userUpdateSchema,
+} from "./users.schema";
+
+export {
+  userAdminOmitSchema,
+  userArrayReturnSchema,
+  userCreateSchema,
+  userReturnSchema,
+  userUpdateSchema,
+};
