9c218aae2bfb709aae9e7364668e4fb87986316c
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-02 16:27:21 -0300

    feat: controller index

---
+++ src/controllers/index.ts
@@ -0,0 +1,13 @@
+import {
+  createUserController,
+  deleteUserController,
+  readUserController,
+  updateUserController,
+} from "./users.controller";
+
+export {
+  createUserController,
+  deleteUserController,
+  readUserController,
+  updateUserController,
+};
