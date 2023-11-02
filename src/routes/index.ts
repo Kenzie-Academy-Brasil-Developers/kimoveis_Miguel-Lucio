f35483c1e90298a51873a5e56693d4b55535c014
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-02 17:22:13 -0300

    feat: implementing loginRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { usersRoutes } from "./users.route";
+import { loginRoutes } from "./login.route";
 
 export const routes: Router = Router();
 
 routes.use("/users", usersRoutes);
+routes.use("/login", loginRoutes);
