89a76cc139625ba5834536088a2bc22930e5be3b
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-03 18:20:55 -0300

    feat: readCategoriesController

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
-import { createCategoryService } from "../services";
+import { createCategoryService, readCategoriesService } from "../services";
 import { Category } from "../entities";
+import { TCategoryArrayReturn } from "../interfaces";
 
 export const createCategoryController = async (
   req: Request,
@@ -9,4 +10,13 @@ export const createCategoryController = async (
   const newCategory: Category = await createCategoryService(req.body);
 
   return res.status(201).json(newCategory);
+};
+
+export const readCategoriesController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const categories: TCategoryArrayReturn = await readCategoriesService();
+
+  return res.status(200).json(categories);
 };
