f725fbfe601c70106dc7810ac1e07dfa2bdbc9ec
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-03 18:59:41 -0300

    feat: readRealEstateByCategoryController

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
-import { createCategoryService, readCategoriesService } from "../services";
+import {
+  createCategoryService,
+  readCategoriesService,
+  readRealEstateByCategoryService,
+} from "../services";
 import { Category } from "../entities";
 import { TCategoryArrayReturn } from "../interfaces";
 
@@ -19,4 +23,14 @@ export const readCategoriesController = async (
   const categories: TCategoryArrayReturn = await readCategoriesService();
 
   return res.status(200).json(categories);
+};
+
+export const readRealEstateByCategoryController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const id = Number(req.params.id);
+  const category = await readRealEstateByCategoryService(id);
+
+  return res.status(200).json(category);
 };
