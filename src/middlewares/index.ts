ffb23cb36f4fc39521268a8d81a87b6372fa5ed3
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-03 13:46:59 -0300

    fat: implementing verifyCategoryName

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { handleErrors } from "./handleErrors.middleware";
 import { validateBody } from "./validateBody.middleware";
+import { verifyCategoryName } from "./verifyCategoryName.middleware";
 import { verifyEmail } from "./verifyEmail.middleware";
 import { verifyId } from "./verifyId.middleware";
 import { verifyPermissions } from "./verifyPermissions.middleware";
@@ -7,6 +8,7 @@ import { verifyToken } from "./verifyToken.middleware";
 
 export { handleErrors };
 export { validateBody };
+export { verifyCategoryName };
 export { verifyEmail };
 export { verifyId };
 export { verifyPermissions };
