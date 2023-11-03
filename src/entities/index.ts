181c082074e0678660ba16903226a97cc4360d7b
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-03 13:45:43 -0300

    fix: organizing exports

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -4,4 +4,8 @@ import { RealEstate } from "./RealEstate.entity";
 import { Schedule } from "./Schedule.entity";
 import { User } from "./User.entity";
 
-export { Address, Category, RealEstate, Schedule, User };
+export { Address };
+export { Category };
+export { RealEstate };
+export { Schedule };
+export { User };
