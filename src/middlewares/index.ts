5fa300cd9f76fe4177187d68c8c01849620b622a
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-02 21:11:35 -0300

    feat: implementing verifyId, verifyPermissions and verifyToken

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,13 @@
 import { handleErrors } from "./handleErrors.middleware";
 import { validateBody } from "./validateBody.middleware";
 import { verifyEmail } from "./verifyEmail.middleware";
+import { verifyId } from "./verifyId.middleware";
+import { verifyPermissions } from "./verifyPermissions.middleware";
+import { verifyToken } from "./verifyToken.middleware";
 
-export { handleErrors, validateBody ,verifyEmail};
+export { handleErrors };
+export { validateBody };
+export { verifyEmail };
+export { verifyId };
+export { verifyPermissions };
+export { verifyToken };
