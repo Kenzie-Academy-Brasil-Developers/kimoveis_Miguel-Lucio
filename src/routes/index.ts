31173b64d15d71fc69567510eef855c5d8c15e98
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-03 13:48:42 -0300

    feat: implementing categoriesRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { usersRoutes } from "./users.route";
 import { loginRoutes } from "./login.route";
+import { categoriesRoutes } from "./categories.route";
 
 export const routes: Router = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/login", loginRoutes);
+routes.use("/categories", categoriesRoutes);
