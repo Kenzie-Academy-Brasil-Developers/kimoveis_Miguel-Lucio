a2b0a4f1dd5621d80f093308ab7c8aef2a985123
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-11-04 17:59:26 -0300

    feat: schedules Schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -6,6 +6,7 @@ import {
 } from "./categories.schema";
 import { loginBodySchema } from "./login.schema";
 import { realEstateCreateSchema, realEstateSchema } from "./realEstates.schema";
+import { scheduleCreateSchema, scheduleSchema } from "./schedules.schema";
 import {
   userAdminOmitSchema,
   userArrayReturnSchema,
@@ -19,6 +20,7 @@ export { addressCreateSchema, addressSchema };
 export { categorySchema, categoryCreateSchema, categoryArrayReturnSchema };
 export { loginBodySchema };
 export { realEstateCreateSchema, realEstateSchema };
+export { scheduleCreateSchema, scheduleSchema };
 export {
   userAdminOmitSchema,
   userArrayReturnSchema,
